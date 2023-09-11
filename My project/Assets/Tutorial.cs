63ad282b7833d2adad30cfbd1c154742cf85e320
Nasjakii <116678282+Nasjakii@users.noreply.github.com>
2023-09-11 05:38:20 +0200

    Tutorial Setup

--- My project/Assets/Tutorial.cs
+++ My project/Assets/Tutorial.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject textfield;
+    private int textfieldIndex;
+    public TextMeshProUGUI text;
+    public TextMeshProUGUI headline;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            textfieldIndex++;
+        }
+        switch (textfieldIndex)
+        {
+            case 0:
+                text.SetText("aaaa");
+                headline.SetText("Headline 0");
+                break;
+            case 1:
+                text.SetText("bbbb");
+                headline.SetText("Headline 1");
+                break;
+            default:
+                gameObject.SetActive(false);
+                break;
+        }
     }
 }
